8e096aa5283cbe91ed8b554bdc85831940ed981e
Andrew Nowakowski <64051143+andrewmnowakowski@users.noreply.github.com>
2022-12-06 16:47:11 -0800

    changed the styling over to bootstrap and modified the js to better handle fetch and the response

--- main.js
+++ main.js
@@ -1,34 +1,38 @@
-const url = "http://127.0.0.1:8080"
+const URL = "http://127.0.0.1:8080"
+
+const ROOT = document.getElementById("root")
 
 function signUpForm() {
-    let html = `<form id="signupform" onsubmit="submitSignupForm(event)" style="border:1px solid #ccc">
-        <div class="container">
+     ROOT.innerHTML = `<form id="signupform" onsubmit="submitSignupForm(event)">
           <h1>Sign Up</h1>
           <p>Please fill in this form to create an account.</p>
           <hr>
-      
-          <label for="email"><b>Email</b></label>
-          <input type="text" placeholder="Enter Email" name="email" required>
-      
-          <label for="psw"><b>Password</b></label>
-          <input type="password" placeholder="Enter Password" name="psw" required>
-      
-          <label for="psw-repeat"><b>Repeat Password</b></label>
-          <input type="password" placeholder="Repeat Password" name="psw-repeat" required>
           
-          <p>By creating an account you agree to our <a href="#" style="color:dodgerblue">Terms & Privacy</a>.</p>
+          <div class="input-group mb-3">
+            <span class="input-group-text" id="email">Email</span>
+            <input name="email" type="text" class="form-control" placeholder="Enter Email Here" aria-label="email" aria-describedby="email" required>
+          </div>
+          
+          <div class="input-group mb-3">
+            <span class="input-group-text" id="password1">Password</span>
+            <input name="password1" type="password" class="form-control" placeholder="Enter Password" aria-label="password1" aria-describedby="password1" required>
+          </div>
+          
+          <div class="input-group mb-3">
+            <span class="input-group-text" id="password2">Password</span>
+            <input name="password2" type="password" class="form-control" placeholder="Repeat Password" aria-label="password2" aria-describedby="password2" required>
+          </div>
+          
+          <p>By creating an account you agree to our <a href="#">Terms & Privacy</a>.</p>
       
           <div class="clearfix">
-            <button type="button" class="cancelbtn">Cancel</button>
-            <button type="submit" class="signupbtn">Sign Up</button>
+            <button type="reset" class="btn btn-danger">Cancel</button>
+            <button type="submit" class="btn btn-success">Sign Up</button>
           </div>
-        </div>
-        
       </form>`
-    document.getElementById("root").innerHTML = html
 }
 
-function submitSignupForm(event) {
+async function submitSignupForm(event) {
     event.preventDefault()
 
     let form = document.getElementById("signupform").elements
@@ -37,7 +41,25 @@ function submitSignupForm(event) {
         employeePassword: form[1].value
     }
 
-    let response = fetch(url + "/register", {method: "POST", mode: "no-cors", body: JSON.stringify(data)}).then(data => console.log(data))
-    
+    // let response = fetch(URL + "/register", {method: "POST", mode: "cors", body: JSON.stringify(data)})
+    //     .then(response => console.log(response))
+    //     .catch(error => console.log(error))
 
+    console.log( await postData("/register", data))
+    ROOT.innerHTML = `<h3>Thank you for registering</h3>`
+}
+
+async function postData(url="", data={}) {
+    try {
+        const response = await fetch(URL + url, {method: "POST", mode: "cors", body: JSON.stringify(data)})
+
+        return {
+            successful: response.ok,
+            headers: await response.headers,
+            body: await response.json()
+        };
+
+    } catch (e) {
+        console.error(e)
+    }
 }
